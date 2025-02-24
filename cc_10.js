d01aebb911520d90ca4e1ba7050bf2c9cd334ebe
danielaj2004 <danielajoya12@gmail.com>
2025-02-23 21:16:07 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -17,3 +17,19 @@ const prod1 = new Product("Laptop", 101, 1200, 10);
 console.log(prod1.getDetails()); // Product: Laptop, ID: 101, Price: 1200, Stock: 10
 prod1.updateStock(3);
 console.log(prod1.getDetails()); // Product: Laptop, ID: 101, Price: 1200, Stock: 7
+
+// Task 2
+class Order {
+    constructor(orderId, product, quantity) {
+        this.orderId = orderId; // order id
+        this.product = product; // product
+        this.quantity = quantity; // quantity
+        this.product.updateStock(quantity); // update stock
+    }
+    getOrderDetails() {
+        return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.product.price * this.quantity}`;
+    }; // order details
+};// order class
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails()); // Order ID: 501, Product: Laptop, Quantity: 2, Total Price: 2400
+console.log(prod1.getDetails()); // Product: Laptop, ID: 101, Price: 1200, Stock: 5
